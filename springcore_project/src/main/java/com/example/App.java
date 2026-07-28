4e423908883b944c59d9e60189ad68ea39ab80a3
Biborka Bartus <biborkabartus@gmail.com>
2026-07-28 08:07:43 +0200

    Added remaining changes

--- springcore_project/src/main/java/com/example/App.java
+++ springcore_project/src/main/java/com/example/App.java
@@ -1,10 +1,5 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 
@@ -12,10 +7,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        // ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        // HelloSpring obj = (HelloSpring) context.getBean("spring", HelloSpring.class);
-        // obj.getMessage();
-        // ((ClassPathXmlApplicationContext) context).close();
 
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
         HelloSpring obj = (HelloSpring) ctx.getBean(HelloSpring.class);
