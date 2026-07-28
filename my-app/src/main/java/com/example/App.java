1f34500dd6eda2259586d7432fb99267f1bb822c
Biborka Bartus <biborkabartus@gmail.com>
2026-07-28 08:05:11 +0200

    Added changes after PR

--- my-app/src/main/java/com/example/App.java
+++ my-app/src/main/java/com/example/App.java
@@ -1,9 +1,5 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
 public class App {
   public static void main(String[] args) {
     System.out.println("Environment: " + System.getProperty("env"));
