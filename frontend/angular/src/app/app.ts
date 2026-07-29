c052604b537a1b4b91d25cef12b30f3c07b87ccc
Biborka Bartus <biborkabartus@gmail.com>
2026-07-29 16:02:44 +0200

    Chapter 204

--- frontend/angular/src/app/app.ts
+++ frontend/angular/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule, MatToolbarModule, MatIconModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
