88b7e9ae341a87020d7b03235fd23d562c09494e
Biborka Bartus <biborkabartus@gmail.com>
2026-07-30 13:49:27 +0200

    Chapter 205

--- frontend/angular/src/app/components/dog-image.ts
+++ frontend/angular/src/app/components/dog-image.ts
@@ -1,26 +1,34 @@
 import { Component, signal } from '@angular/core';
 import { of, map, catchError, Observable, throwError } from 'rxjs';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dog-image',
   templateUrl: './dog-image.html',
 })
-export class DogImageComponent {
+export class DogImageComponent implements OnInit {
+  constructor(private http: HttpClient) {}
   private apiUrl = 'https://dog.ceo/api/breeds/image/random';
-  readonly dogImage = signal<Observable<string>>(of('https://dog.ceo/api/breeds/image/random'));
-  constructor(private http: HttpClient) {
-    this.dogImage.set(this.fetchImage(this.apiUrl));
+  dogImage = signal('');
+
+  ngOnInit() {
+    this.fetchDogImage().subscribe({
+      next: (imageUrl) => this.dogImage.set(imageUrl),
+      error: (error) => console.error(error),
+    });
   }
 
-  fetchImage(url: string): Observable<string> {
-    this.dogImage.set(
-      this.http.get<{ message: string }>(url).pipe(
-        map((response) => response.message),
-        catchError((error: HttpErrorResponse) => this.handleError(error))
-      )
+  private fetchDogImage(): Observable<string> {
+    this.dogImage.set('');
+    return this.getDogImage();
+  }
+
+  private getDogImage(): Observable<string> {
+    return this.http.get<{ message: string }>(this.apiUrl).pipe(
+      map((response) => response.message),
+      catchError(this.handleError)
     );
-    return this.dogImage();
   }
 
   private handleError(error: HttpErrorResponse) {
