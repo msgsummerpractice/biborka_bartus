87390b292a2a091b49332275f79517aef96ff276
Biborka Bartus <biborkabartus@gmail.com>
2026-08-01 13:40:02 +0200

    Fixed GeneratedValue problem

--- project/src/main/java/com/example/project/model/User.java
+++ project/src/main/java/com/example/project/model/User.java
@@ -2,6 +2,7 @@ package com.example.project.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
@@ -19,7 +20,7 @@ import lombok.ToString;
 public class User {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "lastname")
